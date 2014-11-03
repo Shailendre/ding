1ab7dc08f504d8a778dd3c180e72e26738fe41eb
xerotronx <xerotronx@yahoo.com>
2014-11-03 23:50:59 +0530

    Added Multi OS support and Algo Changed

--- check.php
+++ check.php
@@ -2,5 +2,5 @@
 if(PHP_OS=="Linux") $e = exec("ping -c 2 192.168.40.99", $a, $b);
 else $e = exec("ping 192.168.40.99", $a, $b);
 if ($b==1) {echo "No Internet Connection";}
-else{echo $e;}
+else{echo $e;} 
 ?>
